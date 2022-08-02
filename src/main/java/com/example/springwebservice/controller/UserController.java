e4ff2edc85a3589769900f526c65a0666f8a9ec3
BillYang <BillYang@systex.com>
2022-08-02 13:52:56 +0800

    fix error

--- src/main/java/com/example/springwebservice/controller/UserController.java
+++ src/main/java/com/example/springwebservice/controller/UserController.java
@@ -14,7 +14,7 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/all")
+    @GetMapping()
     public List<User> getAllUsers() {
         return null;
     }
