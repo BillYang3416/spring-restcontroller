4d5b4df86de1dd14f4200ad577d9e9206c53ab2c
BillYang <BillYang@systex.com>
2022-08-02 08:11:40 +0800

    init

---
+++ src/main/java/com/example/springwebservice/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.example.springwebservice.controller;
+
+import com.example.springwebservice.model.User;
+import com.example.springwebservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/all")
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable int id) {
+        return null;
+    }
+
+    @PostMapping()
+    public User createUser(@RequestBody User user) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable int id,@RequestBody User user) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public User deleteUser(@PathVariable int id) {
+        return null;
+    }
+} // Class end
---
+++ src/main/java/com/example/springwebservice/service/UserService.java
@@ -0,0 +1,34 @@
+package com.example.springwebservice.service;
+
+import com.example.springwebservice.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    List<User> userList;
+
+
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    public User getUser(int id) {
+        return null;
+    }
+
+    public User createUser(User user) {
+        return null;
+    }
+
+    public User updateUser(int id,User user) {
+        return null;
+    }
+
+    public User deleteUser(int id) {
+        return null;
+    }
+
+} // Class end
